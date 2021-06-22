aeeb449a4a766b092c0e2d6910205f71604b475f
Wdepalma <wdepalma@verizon.net>
2021-06-22 11:22:31 -0400

    more complete soln to pypoll with number formatting

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,13 +4,14 @@ import csv
 Total_Votes = 0
 candidates = []
 Votes = []
+Cand_w_votes = []
+Cand_v_count = []
+Cand_v_percent = []
+vote_tally = []
 tally = {}
 vcount = 0
 maxcount = 0
 percent_count = 0.0
-
-
-
 
 filepath = "Resources/election_data.csv"
 with open(filepath) as election :
@@ -28,22 +29,41 @@ with open(filepath) as election :
 
 print(f"Total Votes = {Total_Votes}")
 
+tv = format(Total_Votes,",")
+print(tv)
+
+
 for  name in candidates:
     tally.update({name:Votes.count(name)})
-    vcount = Votes.count(name)
-    percent_count = (vcount / Total_Votes) * 100
+    vcount = (Votes.count(name))
+    percent_count = (vcount / Total_Votes)
+
+    fmt_vcount = format(vcount,",")
+    fmt_percent_count = format(percent_count,".2%")    
+
+    Cand_w_votes.append(name)
+    Cand_v_count.append(fmt_vcount)
+    Cand_v_percent.append(fmt_percent_count)
+
+
     if vcount > maxcount :
         maxcount = vcount
         winner = name
-    
-    print(f"{name} {vcount}, {percent_count}")
+   
+ #   print(fmt_vcount)
+ #   print(fmt_percent_count)
+ #   print(f"{name} {vcount}, {percent_count}")
 print("Winner is " + winner)
+vote_tally = zip(Cand_w_votes,Cand_v_count,Cand_v_percent)
+#print(vote_tally)
 
 
+for row in vote_tally:
+    print(row[0],row[1],row[2])
 
-print(tally)
-x = tally["Khan"]
-print(x)
+#print(tally)
+#x = tally["Khan"]
+#print(x)
 
 
 
